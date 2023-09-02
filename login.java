92d86e9983669deb111b180dea4d8c7aec67eb13
naveen <sankarapunaveen10@gmail.com>
2023-09-02 10:57:17 +0530

    added some git commands

--- login.java
+++ login.java
@@ -2,4 +2,8 @@ public class login
 git and github
 repositories
 {
+
 }
+
+git log --author="user name"===it will show the commits of that user
+git log --grep="word"===to see commit based on commited word
